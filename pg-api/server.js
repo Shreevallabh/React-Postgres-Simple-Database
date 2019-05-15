ab11728ea06d7f81891a61be6897a34ce2c1126c
Shreevallabh <shreevallabh29@gmail.com>
2019-05-15 14:47:26 -0400

    Added "Delete users" functionality

--- pg-api/server.js
+++ pg-api/server.js
@@ -39,6 +39,7 @@ var User_Uuid = uuidv4();
      {return response.status(400).send(err);}
      else
      {
+         //"table" variable in following line is probably the result returned by running the query
          db.query('INSERT INTO TestUsers (UName, ULevel, Uuid) VALUES ($1,$2,$3)',[User_Name, User_Level, User_Uuid], (err, table)=>{
              if(err) {
                  return response.status(400).send(err);
@@ -48,18 +49,41 @@ var User_Uuid = uuidv4();
                  console.log("Data Inserted");
                 // console.log("UUID is: " + uuidv1());
                  db.end();
-		 response.status(201).send({message:"Data Inserted!"});
+		         response.status(201).send({message:"Data Inserted!"});
              }
          })
      }
  })
-
-
-
-
-
-
-
 });
+
+
+app.post('/api/del-user', function(request, response) {
+    var User_Name = request.body.User_Name;
+    
+     pool.connect((err, db, done) =>{
+         if(err)
+         {return response.status(400).send(err);}
+         else
+         {
+             //"table" variable in following line is probably the result returned by running the query
+             db.query('delete from testusers where uname = $1',[User_Name], (err, table)=>{
+                 if(err) {
+                     return response.status(400).send(err);
+                 }
+                 else
+                 {
+                    console.log(table);
+                     db.end();
+                     response.status(201).send({message:"Data Deleted!"});
+                 }
+             })
+         }
+     })
+    });
+
+
+
+
+
 
   app.listen(PORT, () => console.log('Listening on port ' + PORT));
--- simple-full-stack/src/App.js
+++ simple-full-stack/src/App.js
@@ -24,6 +24,52 @@ class App extends Component {
       console.log('Component has mounted');
     }
 
+
+
+deleteUser(event)
+{
+  //var that = this;
+//alert(this.refs.User_Name.value);
+event.preventDefault();
+let data={
+  User_Name: this.refs.User_Name.value,
+};
+var request = new Request('http://localhost:3000/api/del-user',{
+  method: 'POST',
+  headers: new Headers ({ 'Content-Type': 'application/json'}),
+  body: JSON.stringify(data)
+});
+fetch(request)
+.then(function(response){
+  response.json()
+  .then(function(data) {
+
+    console.log("up");
+    console.log(data);
+    console.log("down");
+  //  if(data.code == "20000")
+    //{
+     // alert("Name doesn't exist");
+   // }
+   
+
+
+    // let users = that.state.users;
+    // //users.push(data);
+    // //console.log(users);
+    // that.setState({
+    //   users:users
+   
+    // })
+  })
+})
+.catch(function(err) {
+  console.log(err);
+})
+}
+
+
+
 addUser(event)
 {
   var that = this;
@@ -56,7 +102,8 @@ fetch(request)
 
 
     let users = that.state.users;
-  //  users.concat(data);
+    //users.push(data);
+    //console.log(users);
     that.setState({
       users:users
    
@@ -71,7 +118,7 @@ fetch(request)
 
 render(){
 let title = this.state.title;
-
+let users = this.state.users;
       return (<div className="App">
       <h1> {title} </h1>
 
@@ -79,6 +126,8 @@ let title = this.state.title;
       <input type="text" ref="User_Name" placeholder="User Name"/>
       <input type="text" ref="User_Level" placeholder="User Level"/>
       <button onClick = {this.addUser.bind(this)}> Add User </button>
+      <button onClick = {this.deleteUser.bind(this)}> Delete User</button>
+      <pre>{JSON.stringify()}</pre>
       </form>
 
     </div>);
